2cbbd68e989b580435910d89f863d7a766e4cf22
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2024-10-06 22:45:59 +0200

    code cleanup

--- motum.py
+++ motum.py
@@ -25,14 +25,6 @@ args = parser.parse_args()
 path = os.path.abspath(args.src)
 host = args.host
 
-if host=='localhost':
-    dst = os.path.join(args.dst, os.path.basename(path) + '/').replace(' ','\ ')
-else:
-    _dst = os.popen(f'ssh {host} "echo {args.dst}" 2> /dev/null').read().strip()
-    dst = os.path.join(_dst, os.path.basename(path) + '/').replace(' ','\ ')
-
-verbose = args.verbose
-
 class Log:
     def __init__(self):
         self.t0 = time.time()
@@ -42,33 +34,45 @@ class Log:
         print(f'[motum {dt:.4e} s]: {msg}', flush=True)
 
 log = Log()
-log(f'{path} -> {host}:{dst}')
 
 def run_cmd(cmd, remote, parser = lambda x: x.read().rstrip()):
     runner = lambda c: parser(os.popen(c))
     if remote and (host!='localhost'):
-        return runner(f'ssh {host} "du -sk {dst}" 2> /dev/null')
+        return runner(f'ssh {host} "{cmd}" 2> /dev/null')
     return runner(cmd)
     
+def getsize(p, r):
+    return run_cmd(f'du -sb {p}', remote=r, parser = lambda x: int(x.read().split()[0]))
+
+# checks if destination path exists
+if run_cmd(f'if [ -d {args.dst} ]; then echo "Y"; fi', remote=True)!='Y':
+    log('Folder {args.dst} does not exists on host')
+    sys.exit(1)
+
+# creates proper dst variable
+_dst = run_cmd(f'echo {args.dst}', True)
+dst = os.path.join(_dst, os.path.basename(path) + '/').replace(' ','\ ')
+
+log(f'{path} -> {host}:{dst}')
+
+verbose = args.verbose
+
 class Bandwidth:
     def __init__(self):
         self.t0 = time.time()
         self.s0 = 0
-        self.full = int(os.popen(f'du -sk "{path}"').read().split()[0])
-        log(f'total size {self.full/1024:.2f} MB')
+        self.full = getsize(path, False)
+        log(f'total size {self.full/1024**2:.2f} MB')
 
     def run(self):
-        if host=='localhost':
-            s1 = int(os.popen(f'du -sk {dst}').read().split()[0])
-        else:
-            s1 = int(os.popen(f'ssh {host} "du -sk {dst}" 2> /dev/null').read().split()[0])
+        s1 = getsize(dst, True)
         t1 = time.time()
         dt = t1 - self.t0
         ds = s1 - self.s0
         self.t0, self.s0 = t1, s1
         if verbose>1:
-            log(f'transferred size {ds/1024:.2f} MB')
-        sys.stdout.write(f'\r[motum]: effective bandwidth {ds/dt/1024:.2f} MB/sec ; {int(s1/self.full*100): <3d} % completed')
+            log(f'transferred size {ds/1024**2:.2f} MB')
+        sys.stdout.write(f'\r[motum]: effective bandwidth {ds/dt/1024**2:.2f} MB/sec ; {int(s1/self.full*100): <3d} % completed')
         sys.stdout.flush()
     
     def __call__(self):
@@ -77,36 +81,6 @@ class Bandwidth:
                 self.run()
             except:
                 pass
-
-# def bandwidth_init():
-#     t0 = time.time()
-#     tot0 = 0
-#     full = int(os.popen(f'du -sk "{path}"').read().split()[0])
-#     log(f'total size {full/1024:.2f} MB')
-    
-#     def run():
-#         if host=='localhost':
-#             tot = int(os.popen(f'du -sk {dst}').read().split()[0])
-#         else:
-#             tot = int(os.popen(f'ssh {host} "du -sk {dst}" 2> /dev/null').read().split()[0])
-#         if verbose>1:
-#             log(f'transferred size {tot/1024:.2f} MB')
-#         t1 = time.time()
-#         dt = t1 - t0
-#         ds = tot - tot0
-#         t0 = t1
-#         tot0 = tot
-#         sys.stdout.write(f'\r[motum]: effective bandwidth {ds/dt/1024:.2f} MB/sec ; {int(tot/full*100): <3d} % completed')
-#         sys.stdout.flush()
-    
-#     def inner():
-#         if not args.dry_run:
-#             run()
-#             # try:
-#                 # run()
-#             # except:
-#                 # pass
-#     return inner
 
 def create_rsync_cmd(arg):
     cmd = ['rsync','-aczR','-pgot','--partial','--inplace']
@@ -125,16 +99,7 @@ class MoveTask:
         self.arg = f'{path}/./{os.path.relpath( arg, path)}'
 
     def __call__(self):
-        cmd = ['rsync','-aczR','-pgot','--partial','--inplace']
-        if args.dry_run:
-            cmd += ['--dry-run']
-        cmd += [self.arg]
-
-        if host=='localhost':
-            cmd += [dst]
-        else:
-            cmd += ['-e','ssh',f'{host}:{dst}']
-
+        cmd = create_rsync_cmd(self.arg)
         if verbose>0:
             log(f'starting transfer of {self.arg}')
             log(f'{" ".join(cmd)}')
@@ -200,7 +165,7 @@ def create_tree_checksum(p):
 if args.checksum:
     ds = 0
     for _s, _p, _r in zip([+1,-1],[path,dst],[False,True]):
-        size = int(run_cmd(f'du -sk {_p}', remote=_r).split()[0])
+        size = getsize(_p, _r)
         log(f'{size} size of {_p}')
         ds = ds + _s * size
     if ds!=0.0:
@@ -210,15 +175,22 @@ if args.checksum:
     cmd = "find . -type f"
     lf = [run_cmd(f'cd {_p}; {cmd}', remote=_r, parser = lambda x: x.readlines()) for _p, _r in zip([path,dst],[False,True])]
     not_copied = []
+    partial = []
     for f in lf[0]:
         if not f in lf[1]:
             not_copied += [f.rstrip()]
+        #else:
+        #    s1 = getsize(os.path.join(path,f),False)
+        #    s2 = getsize(os.path.join(dst,f),True)
+        #    if abs(s1-s2)>8:
+        #        log(f"WARNING: sizes of file {f} do not match = {s1} vs {s2}")
+
     if not_copied:
         for f in not_copied:
             log(f"Missing file {f}")
             if input("Do you want to copy it? [y/n]")=='y':
                 MoveTask(path+'/'+f)()
-        # sys.exit(999)
+
     create_tree_checksum(path)
 else:
     bandwidth = Bandwidth()
